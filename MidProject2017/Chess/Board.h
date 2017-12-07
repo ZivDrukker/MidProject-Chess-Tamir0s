9758fa2782dbe9452bab0dd9abe55b6165f61ebd
ZivDrukker <34239065+ZivDrukker@users.noreply.github.com>
2017-12-07 20:41:52 +0200

    ERR Fix
    Testing ERR fix

--- MidProject2017/Chess/Board.h
+++ MidProject2017/Chess/Board.h
@@ -6,22 +6,6 @@
 #include "Soldier.h"
 
 #define SIZE 8
-#define A_ASCII 97
-#define ONE_ASCII 49
-
-#define NULL_PIECE '#'
-#define KING_BLACK 'k'
-#define QUEEN_BLACK 'q'
-#define BISHOP_BLACK 'b'
-#define ROOK_BLACK 'r'
-#define KNIGHT_BLACK 'n'
-#define PAWN_BLACK 'p'
-#define KING_WHITE 'K'
-#define QUEEN_WHITE 'Q'
-#define BISHOP_WHITE 'B'
-#define ROOK_WHITE 'R'
-#define KNIGHT_WHITE 'N'
-#define PAWN_WHITE 'P'
 
 using std::string;
 using std::cout;
@@ -35,11 +19,12 @@ private:
 	void setBoard(string str);
 
 public:
-	Soldier*** _gameBoard;
+	Soldier* _board[SIZE][SIZE];
 
 	Board(string str);
 	~Board();
 	Soldier* getCell(string cell);
-	void setCell(Soldier* piece, string cell);
-	Soldier* operator()(int x, int y);
+	void setCell(Soldier*);
+	Soldier* operator=(Soldier*);
+	Soldier* operator[](int x);
 };
